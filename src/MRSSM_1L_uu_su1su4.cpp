137b4cf6dcdd7f11cf882f6a2f2d021efdd280e4
Sebastian Liebschner <Sebastian-Liebschner@gmx.de>
2016-05-13 21:25:13 +0200

    sign change in sgluon mass, no big effect

--- src/MRSSM_1L_uu_su1su4.cpp
+++ src/MRSSM_1L_uu_su1su4.cpp
@@ -5,7 +5,7 @@ double MsquaredRealMRSSMVirt_uu_suLsuR(double alphaS, double MassSq, double Mass
                 double MassTop, double mu, double FiniteGs, 
                 double Dminus4, double divergence)
 {
-	double MassphiO = sqrt(pow(MasssigmaO,2) - 4 * pow(MassGlu,2));
+	double MassphiO = sqrt(pow(MasssigmaO,2) + 4 * pow(MassGlu,2));
 	ltini();
 	setmudim(pow(mu,2));
 	setlambda(divergence);
