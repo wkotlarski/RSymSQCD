d235ea518ffe5cd7364df7ea7b0f2bf2f3b6cb98
Sebastian Liebschner <Sebastian-Liebschner@gmx.de>
2016-09-24 22:29:01 +0200

    directory with amplitudes added

---
+++ amplitudes/MSSM_Tree_uu_su1su4.h
@@ -0,0 +1,10 @@
+#ifndef __MSQUARED_Tree_MSSM_H
+#define __MSQUARED_Tree_MSSM_H
+
+#include <math.h>
+using namespace std;
+
+
+double matrixMSSMTree_uu_suLsuR(double, double, double, double, double, double);
+
+#endif
