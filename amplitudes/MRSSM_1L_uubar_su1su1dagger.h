a914b01ef405f38b17d72dddf236d7bb3a3bf4b0
Sebastian Liebschner <Sebastian-Liebschner@gmx.de>
2016-10-12 16:10:49 +0200

    amplitudes for squark antisquark production

---
+++ amplitudes/MRSSM_1L_uubar_su1su1dagger.h
@@ -0,0 +1,15 @@
+#ifndef __MSQUARED_1L_MRSSM_UUBAR_SUSUDAGGER_H
+#define __MSQUARED_1L_MRSSM_UUBAR_SUSUDAGGER_H
+
+#include <math.h>
+#include <complex>
+#include "clooptools.h"
+using namespace std;
+
+typedef complex<double> dcomp;
+
+double matrixMRSSMVirt_uubar_suLsuLdagger(double, double, double, double, double, double, 
+                         double, double, double, double, double,
+                         double);
+
+#endif
---
+++ amplitudes/MRSSM_Tree_GG_su1su1dagger.h
@@ -0,0 +1,9 @@
+#ifndef __MSQUARED_TREE_MRSSM_GG_SUSUDAGGER_H
+#define __MSQUARED_TREE_MRSSM_GG_SUSUDAGGER_H
+
+#include <math.h>
+using namespace std;
+
+double matrixMRSSMTree_GG_suLsuLdagger(double, double, double, double, double, double);
+
+#endif
