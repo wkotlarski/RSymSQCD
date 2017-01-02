466abf777968cc63775f7ee7c5d9f6444647c5e6
Wojciech Kotlarski <wojtek.kotlarski@gmail.com>
2017-01-02 07:50:37 +0100

    move around sgluon ME elements

--- src/matrix_elements_and_xsections/mrssm_gg_OO_tree_xsec.cpp
+++
@@ -1,6 +0,0 @@
-inline double Process::matrixSgluonTree_gg_OO( double s ) {
-   double b = sqrt( 1. - 4. * m1*m1/s );
-   double a = pdf->alphasQ( mu_r );
-   return 3. * pi * pow(a, 2)/(32. * s) 
-      * (27.*b - 17*pow(b, 3) + 6.*(-3. + 2. * pow(b, 2) + pow(b, 4) ) * atanh(b) ) ;
-}
--- src/matrix_elements_and_xsections/mrssm_uubar_OO_tree_xsec.cpp
+++
@@ -1,4 +0,0 @@
-inline double Process::matrixSgluonTree_qqbar_OO( double s ) {
-   double a = pdf->alphasQ( mu_r );
-   return 2. * pi * pow(a, 2)/(9. * s) * pow(1. - 4. * m1*m1/s, 3./2. );
-}
