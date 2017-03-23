c44e704b19d609d82d7fe480262df322718c82fc
Wojciech Kotlarski <plgwkotlarski@login01.pro.cyfronet.pl>
2017-03-23 16:40:52 +0100

    added 'DR' to the name of function with gu_suLsuR ME; removed Theta from this function

--- src/matrix_elements_and_xsections/mrssm_gu_suLsuRubar_hard-DR.cpp
+++ src/matrix_elements_and_xsections/mrssm_gu_suLsuRubar_hard-DR.cpp
@@ -1,14 +1,12 @@
-double Process::matrixMRSSMHard_gu_suLsuRubar( std::vector< double* >& p ) {
+double Process::matrixMRSSMHard_gu_suLsuRubar_DR( std::vector< double* >& p ) {
    double Alfas = pdf->alphasQ( mu_r );
    double Alfas2 = pow(Alfas, 2);
-   double Theta = MassGlu > m2 ? 1 : 0;
    double k12 = p[0][0]*p[1][0]-p[0][1]*p[1][1]-p[0][2]*p[1][2]-p[0][3]*p[1][3];
    double k35 = p[2][0]*p[4][0]-p[2][1]*p[4][1]-p[2][2]*p[4][2]-p[2][3]*p[4][3];
    double k45 = p[3][0]*p[4][0]-p[3][1]*p[4][1]-p[3][2]*p[4][2]-p[3][3]*p[4][3];
    double k13 = p[0][0]*p[2][0]-p[0][1]*p[2][1]-p[0][2]*p[2][2]-p[0][3]*p[2][3];
    double k14 = p[0][0]*p[3][0]-p[0][1]*p[3][1]-p[0][2]*p[3][2]-p[0][3]*p[3][3];
    double S12 = 2.*k12;
-   Theta *= S12 > (MassGlu + m1)^2 ? 1 : 0;
    double S35 = m1*m1 + 2*k35;
    double S45 = m2*m2 + 2*k45;
    double T = m1*m1 - 2.*k13;
