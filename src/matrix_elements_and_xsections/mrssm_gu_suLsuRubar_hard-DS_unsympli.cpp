b69f30f1abe115867ebe1d7034de836f261f7061
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2022-04-29 11:29:56 +0200

    unify const with development branch

--- src/matrix_elements_and_xsections/mrssm_gu_suLsuRubar_hard-DS_unsympli.cpp
+++ src/matrix_elements_and_xsections/mrssm_gu_suLsuRubar_hard-DS_unsympli.cpp
@@ -1,4 +1,4 @@
-double Process::matrixMRSSMHard_gu_suLsuRubar_DS( const std::vector< double* >& p ) const {
+double Process::matrixMRSSMHard_gu_suLsuRubar_DS(std::vector<double*> const& p) const {
    double Alfas = pdf->alphasQ( mu_r );
    double Alfas2 = pow(Alfas, 2);
    double Theta = MassGlu > m2 ? 1 : 0;
