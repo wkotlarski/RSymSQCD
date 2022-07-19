b0db937731c233ec42fe7841d88d068e98af0552
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2022-07-20 00:20:06 +0200

    missing mathematical functions for previous commit

--- include/mathematica_wrapper.hpp
+++ include/mathematica_wrapper.hpp
@@ -3,23 +3,6 @@
 
 #include "Li2.hpp"
 
-template <typename T>
-constexpr std::complex<T> Sqr(const std::complex<T>& a) noexcept
-{
-   return a * a;
-}
-
-template <typename T, class = std::enable_if_t<std::is_arithmetic<T>::value,T>>
-constexpr T Sqr(T a) noexcept
-{
-   return a * a;
-}
-
-template <typename Base, typename Exponent>
-inline Base Power(Base base, Exponent exp) {
-   return std::pow(base, exp);
-}
-
 inline double Sqrt(double a) {
    return std::sqrt(a);
 }
@@ -48,6 +31,42 @@ inline double PolyLog(int i, double x) {
       throw std::invalid_argument("We handle only dilogarithms");
    }
    return polylogarithm::Li2(x);
+}
+
+template <typename Base, typename Exponent>
+Base Power(Base base, Exponent exp) noexcept
+{
+   return std::pow(base, exp);
+}
+
+template <typename T>
+constexpr std::complex<T> Sqr(const std::complex<T>& a) noexcept
+{
+   return a * a;
+}
+
+template <typename T, class = std::enable_if_t<std::is_arithmetic<T>::value,T>>
+constexpr T Sqr(T a) noexcept
+{
+   return a * a;
+}
+
+template <typename Base>
+constexpr Base Power2(Base b) noexcept
+{
+   return b * b;
+}
+
+template <typename Base>
+constexpr Base Power3(Base b) noexcept
+{
+   return b * b * b;
+}
+
+template <typename Base>
+constexpr Base Power4(Base b) noexcept
+{
+   return Power2(Power2(b));
 }
 
 #endif // MATHEMATICA_WRAPPER_HPP_
