d7c1a16064a79eb203c8e18580d1f0ba3489a168
Wojtek <wojciech.kotlarski@posteo.net>
2022-07-28 12:19:01 +0200

    use one script to test all outputs

--- test/compare_json_outputs.rb
+++ test/compare_json_outputs.rb
@@ -1,21 +1,27 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'securerandom'
 
-file_ref = File.read(ARGV[0])
-file_new = File.read(ARGV[1])
-data_hash_ref = JSON.parse(file_ref)
-data_hash_new = JSON.parse(file_new)
+uuid = SecureRandom.uuid
+system("#{ARGV[0]}/RSymSQCD --card #{ARGV[1]} --json-outputfile-name=#{uuid}")
 
-data_hash_ref["cross sections"].each do |key, _|
-  data_hash_ref["cross sections"][key].each do |key2, _|
-    xsec_ref = data_hash_ref["cross sections"][key][key2]["res"]
-    err_ref  = data_hash_ref["cross sections"][key][key2]["err"]
-    xsec_new = data_hash_new["cross sections"][key][key2]["res"]
-    err_new  = data_hash_new["cross sections"][key][key2]["err"]
-    puts "Channel #{key} #{key2} difference: #{xsec_ref} +/- #{err_ref} vs. #{xsec_new} vs. #{err_new} (\#σ = #{(xsec_ref-xsec_new).abs/(3.0*Math.sqrt(err_ref**2 + err_new**2))})"
-    if (xsec_ref-xsec_new).abs > 3.0*Math.sqrt(err_ref**2 + err_new**2)
-      exit(1)
-    end
+file_ref     = File.read(ARGV[2])
+file_to_test = File.read(uuid)
+content_ref = JSON.parse(file_ref)
+content_new = JSON.parse(file_to_test)
+
+content_ref["cross sections"].each do |key, _|
+  content_ref["cross sections"][key].each do |key2, _|
+    xsec_ref = content_ref["cross sections"][key][key2]["res"]
+    err_ref  = content_ref["cross sections"][key][key2]["err"]
+    xsec_new = content_new["cross sections"][key][key2]["res"]
+    err_new  = content_new["cross sections"][key][key2]["err"]
+
+    exit 1 if err_ref < 0 || err_new < 0
+
+    puts "#{key} #{key2}: #{xsec_ref} v. #{xsec_new} (1-ratio: #{1-xsec_ref/xsec_new}, max_error: #{[err_ref, err_new].max}"
+
+    exit 1 if (xsec_ref-xsec_new).abs > 3.0*Math.sqrt(err_ref**2 + err_new**2)
   end
 end
