868abbc15529de46a0b47e47570a853cd460a0d7
Wojtek <wojciech.kotlarski@posteo.net>
2022-08-03 11:34:05 +0200

    removed unused constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -10,14 +10,12 @@ using boost::math::double_constants::pi;
 using boost::math::double_constants::pi_sqr;
 using boost::math::double_constants::pi_cubed;
 using boost::math::double_constants::two_pi;
-using boost::math::double_constants::euler;
-using boost::math::double_constants::e;
 
 // physics
 
-static constexpr double to_fb {3.893793656e+11};
+constexpr double to_fb {3.893793656e+11};
 // SU(3) group factors
-static constexpr double CF {4/3.};
-static constexpr double CA {3.};
+constexpr double CF {4/3.};
+constexpr double CA {3.};
 
 #endif /* CONSTANTS_HPP_ */
