4db0462f959b9cfeb8239c26d591cedaf72c8c5f
Wojtek <wojciech.kotlarski@posteo.net>
2022-08-07 11:32:22 +0200

    secure ruby script

--- ancillary/parse_jason_output.rb
+++ ancillary/parse_jason_output.rb
@@ -2,6 +2,16 @@
 
 require 'json'
 
+if ARGV.length != 1
+  puts "Expected one argument (JSON file name). Received #{ARGV.length}"
+  exit 1
+end
+
+if !File.file?(ARGV[0])
+  puts "No such file #{ARGV[0]}"
+  exit 1
+end
+
 content = JSON.parse(File.read(ARGV[0]))
 
 xsec = 0.0
