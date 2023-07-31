cb3ffd78d1694806cae40d3df30d1a72c123fab3
Wojciech Kotlarski <wojciech.kotlarski@posteo.net>
2023-07-31 10:10:15 +0200

    updated result printout

--- src/utils.cpp
+++ src/utils.cpp
@@ -11,28 +11,31 @@ void print_to_terminal(
    std::string_view str,
    std::array<double, 3> const& tree, std::array<double, 3> const& virt, std::array<double, 3> const& soft, std:: array<double,3> const& hard)
 {
+   static constexpr int width = 19;
+   static constexpr int precision = 5;
+   static constexpr std::string_view line = "----------------------------------------------------------------------\n";
+
    std::cout << "\nResults for subprocess " << str << '\n';
    std::cout << std::scientific;
-   // print out LO run statistics
-   std::cout << "---------------------------------------------------------------\n";
-   std::cout << std::setprecision(5);
-   std::cout << std::setw(12) << "tree:" << std::setw(13) << tree.at(0)
+   std::cout << line;
+   std::cout << std::setprecision(precision);
+   std::cout << std::setw(width) << "born (B):" << std::setw(13) << tree.at(0)
          << " +/- " << std::setprecision(1) << tree.at(1)
          << " fb ( p-value = " << std::setw(8) << tree.at(2) << " )\n";
-   std::cout << std::setprecision(5);
-   std::cout << std::setw(12) << "virtual:" << std::setw(13) << virt.at(0) << " +/- "
+   std::cout << std::setprecision(precision);
+   std::cout << std::setw(width) << "virtual (V):" << std::setw(13) << virt.at(0) << " +/- "
            << std::setprecision(1) << virt.at(1) << " fb ( p-value = "
            << std::setw(8) << virt.at(2) << " )\n";
 
-      std::cout << std::setprecision(5);
-      std::cout << std::setw(12) << "real (soft):" << std::setw(13) << soft.at(0) << " +/- " << std::setprecision(1) << soft.at(1)
+      std::cout << std::setprecision(precision);
+      std::cout << std::setw(width) << "real (S+HC):" << std::setw(13) << soft.at(0) << " +/- " << std::setprecision(1) << soft.at(1)
            << " fb ( p-value = " << std::setw(8) << soft.at(2) << " )\n";
-      std::cout << std::setprecision(5);
-      std::cout << std::setw(12) << "real (hard):" << std::setw(13) << hard.at(0) << " +/- " << std::setprecision(1) << hard.at(1)
+      std::cout << std::setprecision(precision);
+      std::cout << std::setw(width) << "real (HnC):" << std::setw(13) << hard.at(0) << " +/- " << std::setprecision(1) << hard.at(1)
            << " fb ( p-value = " << std::setw(8) << hard.at(2) << " )\n";
-      std::cout << "---------------------------------------------------------------\n";
-      std::cout << std::setprecision(5);
-      std::cout << std::setw(12) << "sum:" << std::setw(13)
+      std::cout << line;
+      std::cout << std::setprecision(precision);
+      std::cout << std::setw(width) << "sum (B+V+S+HC+HnC):" << std::setw(13)
            << tree.at(0) + virt.at(0) + hard.at(0) + soft.at(0)
            << " +/- " << std::setprecision(1) << std::sqrt(pow(tree.at(1),2) +
            Sqr(virt.at(1)) + Sqr(hard.at(1)) +
@@ -45,7 +48,7 @@ void print_to_terminal(std::string_view str, std::array<double, 3> const& tree) 
    //print out LO run statistics
    std::cout << "---------------------------------------------------------------\n";
    std::cout << std::setprecision(5);
-   std::cout << std::setw(12) << "tree:" << std::setw(13) << tree.at(0)
+   std::cout << std::setw(12) << "born:" << std::setw(13) << tree.at(0)
          << " +/- " << std::setprecision(1) << tree.at(1)
          << " fb ( p-value = " << std::setw(8) << tree.at(2) << " )\n";
 }
